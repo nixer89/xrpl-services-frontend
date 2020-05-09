3a2f20fa7474765ae0403cee087aff373beb9932
nixer89 <pbnixer@gmail.com>
2020-05-09 10:27:04 +0200

    update

--- src/app/components/tools/norippleCheck.ts
+++ src/app/components/tools/norippleCheck.ts
@@ -3,7 +3,8 @@ import { Encode } from 'xrpl-tagged-address-codec';
 import { Observable, Subscription, Subject } from 'rxjs';
 import { XummPostPayloadBodyJson, XummJsonTransaction } from 'xumm-api';
 import { GoogleAnalyticsService } from '../../services/google-analytics.service';
-import { AccountInfoChanged, XrplAccountChanged } from 'src/app/utils/types';
+import { AccountInfoChanged } from 'src/app/utils/types';
+import { WebSocketSubject, webSocket } from 'rxjs/webSocket';
 
 interface NoRippleCheck {
   problem: string,
@@ -12,7 +13,8 @@ interface NoRippleCheck {
 
 @Component({
   selector: 'norippleCheck',
-  templateUrl: './norippleCheck.html'
+  templateUrl: './norippleCheck.html',
+  styleUrls: ['./norippleCheck.css']
 })
 export class NoRippleCheckComponent implements OnInit, OnDestroy {
 
@@ -31,23 +33,26 @@ export class NoRippleCheckComponent implements OnInit, OnDestroy {
   xrplAccountInput: string;
 
   originalAccountInfo:any;
-  testMode:boolean = false;
+  isTestMode:boolean = false;
 
   isGateWayRole:boolean = false;
+  isUserRole:boolean = false;
 
   private accountInfoChangedSubscription: Subscription;
   private transactionSuccessfullSubscription: Subscription;
 
   validAddress:boolean = false;
+  loadingProblems:boolean = false;
 
-  problems:NoRippleCheck[];
+  displayedColumns: string[] = ['problem', 'fix'];
+  problemsAndTransactions:NoRippleCheck[] = [];
 
   ngOnInit() {
     this.accountInfoChangedSubscription = this.accountInfoChanged.subscribe(accountData => {
       console.log("account info changed received: " + JSON.stringify(accountData));
       if(accountData && accountData.info && !accountData.info.error) {
         this.originalAccountInfo = accountData.info;
-        this.testMode = accountData.mode;
+        this.isTestMode = accountData.mode;
 
         this.xrplAccountInput = this.originalAccountInfo.Account;
       } else {
@@ -68,12 +73,59 @@ export class NoRippleCheckComponent implements OnInit, OnDestroy {
       this.transactionSuccessfullSubscription.unsubscribe();
   }
 
+  loadProblems() {
+    if(this.xrplAccountInput && this.validAddress) {
+      this.googleAnalytics.analyticsEventEmitter('load_noripple_check', 'noripple_check', 'noripple_check_component');
+      this.loadingProblems = true;
+
+      //console.log("connecting websocket");
+      let websocket:WebSocketSubject<any> = webSocket(this.isTestMode ? 'wss://testnet.xrpl-labs.com' : 'wss://xrpl.ws');
+
+      websocket.asObservable().subscribe(async message => {
+        console.log("websocket message: " + JSON.stringify(message));
+        if(message && message.status && message.status === 'success' && message.type && message.type === 'response' && message.result && message.result.problems && message.result.transactions) {
+          let problems:string[] = message.result.problems;
+          let transactions:any[] = message.result.transactions;
+
+          for(let i = 0; i < problems.length; i++) {
+            this.problemsAndTransactions.push({problem: problems[i], txJson: transactions[i]});
+          }
+
+          console.log("problemsAndTransactions: " + JSON.stringify(this.problemsAndTransactions));
+
+          websocket.unsubscribe();
+          websocket.complete();
+          websocket = null;
+
+          this.loadingProblems = false;
+        } else {
+          this.problemsAndTransactions = [];
+
+          websocket.unsubscribe();
+          websocket.complete();
+          websocket = null;
+
+          this.loadingProblems = false;
+        }
+      });
+
+      let noripple_check_command:any = {
+        command: "noripple_check",
+        account: this.xrplAccountInput.trim(),
+        role: this.isGateWayRole ? 'gateway' : 'user',
+        ledger_index: "validated",
+        transactions: true
+      }
+
+      websocket.next(noripple_check_command);
+    }
+  }
+
   sendPayloadToXumm(txJson: XummJsonTransaction) {
 
     this.googleAnalytics.analyticsEventEmitter('no_ripple_check', 'sendToXumm', 'no_ripple_check_component');
 
     if(txJson) {
-      delete txJson.Account;
       delete txJson.Sequence;
       delete txJson.Fee;
 
@@ -82,7 +134,7 @@ export class NoRippleCheckComponent implements OnInit, OnDestroy {
       }
   
       payload.custom_meta = {};
-      payload.custom_meta.instruction = "- Issuer Address: " +this.issuerAccountInput.trim();
+      payload.custom_meta.instruction = "- Sign with: " +this.xrplAccountInput.trim();
   
       this.onPayload.emit(payload);
     }
@@ -90,6 +142,9 @@ export class NoRippleCheckComponent implements OnInit, OnDestroy {
 
   checkChanges() {
     this.validAddress = this.xrplAccountInput && this.xrplAccountInput.trim().length > 0 && this.isValidXRPAddress(this.xrplAccountInput.trim());
+
+    if(this.validAddress)
+      this.loadProblems();
   }
 
   isValidXRPAddress(address: string): boolean {
