ab793ebdfee01c07150fa10eeea8b1bcd7b9fc36
nixer89 <pbnixer@gmail.com>
2021-09-08 12:12:35 +0200

    v.2.9.5: some more fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { XrplTransactionsComponent } from './routes/xrpl-transactions';
 import { Tools } from './routes/tools';
-import { GenericBackendDefinition } from './routes/generic-backend';
+//import { GenericBackendDefinition } from './routes/generic-backend';
 import { PrivacyComponent } from './routes/specific/privacy';
 import { TermsComponent } from './routes/specific/terms';
 import { EasyToken } from './routes/easyToken';
@@ -12,7 +12,7 @@ export const routes: Routes = [
     {path: '', component: XrplTransactionsComponent},
     {path: 'tokens', component: EasyToken},
     {path: 'tools', component: Tools},
-    {path: 'generic-backend', component: GenericBackendDefinition},
+    //{path: 'generic-backend', component: GenericBackendDefinition},
     {path: 'statistics', component: Statistics},
     {path: 'privacy', component: PrivacyComponent},
     {path: 'terms', component: TermsComponent},
