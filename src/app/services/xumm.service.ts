ada2df315d0d5075b7fa1c6222cf97613e88ce05
nixer89 <pbnixer@gmail.com>
2021-11-03 22:05:50 +0100

    v.3.0.2: fix local host backend...

--- src/app/services/xumm.service.ts
+++ src/app/services/xumm.service.ts
@@ -7,7 +7,7 @@ import { GenericBackendPostRequest, TransactionValidation } from '../utils/types
 export class XummService {
     constructor(private app: AppService) {}
 
-    isTestMode = true;
+    isTestMode = false;
     xummBackendURL = this.isTestMode ? 'http://localhost:4001' : 'https://api.xumm.community';
 
     async submitPayload(payload:GenericBackendPostRequest): Promise<XummTypes.XummPostPayloadResponse> {
