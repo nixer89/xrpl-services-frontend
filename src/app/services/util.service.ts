baf0779402b08533fd586a2781ffe1d41a934c99
nixer89 <pbnixer@gmail.com>
2022-02-10 12:02:58 +0100

    v.4.1.1: fix raw transaction view

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -7,7 +7,7 @@ export class UtilService {
 
     async getTransactionTypes(): Promise<any> {
         let dirList:any[] = await this.app.get('https://api.github.com/repos/xrplf/xrpl-dev-portal/contents/content/references/protocol-reference/transactions/transaction-types');
-        let relevantFiles:any[] = dirList.filter(r => r.name.match(/^[a-zA-Z]+\.md$/))
+        let relevantFiles:any[] = dirList.filter(r => !r.name.includes('.ja.') && r.name.match(/^[a-zA-Z]+\.md$/))
 
         let sources:any[] = [];
     
@@ -22,21 +22,23 @@ export class UtilService {
         for(let i = 0; i < sources.length; i++) {
             let source:string = sources[i];
 
-            result.push({
-                transactionType: source.match(/^# ([a-zA-Z]+)/gm)[0].slice(2),
-                docLink: 'https://xrpl.org/' + relevantFiles[i].name.split('.')[0] + '.html',
-                requiresAmendment: source.match(/^(_\(Requires the).*(:not_enabled:.\).)$/gm) != null,
-                codeSamples: source.split(`\n`).join(' ').match(/```.+?```/gm)
-                    .map(s => s.split('```')[1].trim().replace(/^json[\n]*/gm, ''))
-                    .map(s => s.replace(/,[ \t\n\\t\\n]*}$/, '}'))
-                    .map(s => {
-                        try {
-                            return JSON.parse(s)
-                        } catch (e) {
-                            return s
-                        }
-                    })
-            });
+            if(source.split(`\n`)?.join(' ')?.match(/```.+?```/gm)) {
+                result.push({
+                    transactionType: source.match(/^# ([a-zA-Z]+)/gm)[0].slice(2),
+                    docLink: 'https://xrpl.org/' + relevantFiles[i].name.split('.')[0] + '.html',
+                    requiresAmendment: source.match(/^(_\(Requires the).*(:not_enabled:.\).)$/gm) != null,
+                    codeSamples: source.split(`\n`).join(' ').match(/```.+?```/gm)
+                        .map(s => s.split('```')[1].trim().replace(/^json[\n]*/gm, ''))
+                        .map(s => s.replace(/,[ \t\n\\t\\n]*}$/, '}'))
+                        .map(s => {
+                            try {
+                                return JSON.parse(s)
+                            } catch (e) {
+                                return s
+                            }
+                        })
+                });
+            }
         }
 
         return result;
